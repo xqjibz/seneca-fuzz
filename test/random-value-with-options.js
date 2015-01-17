0733f4609e23a456618aa0612c7822b46a221d2e
rmyers <ccrider@whiterose.net>
2015-01-17 02:59:44 -0500

    updated logic fixes and tests with options.

--- test/random-value-with-options.js
+++ test/random-value-with-options.js
@@ -1,7 +1,23 @@
-var     randomValue = require('../lib/random-value')()
+var randomOptions = {
+        arrayMax : 8
+    ,   integerMax : 6
+    ,   floatMax : 7
+    ,   arrayMin : 5
+    ,   integerMin : 3
+    ,   floatMin : 0.1
+    ,   stringMin : 9
+    ,   dateEnd : new Date(2012,3,14)
+    ,   dateStart : new Date(1999,2,7,18,0,0)
+    ,   stringMax : 42
+    ,   objectMin : 3
+    ,   objectMax : 4
+
+}
+
+var     randomValue = require('../lib/random-value')(randomOptions)
     ,   expect          = require('chai').expect
 
-describe('randomValue() with default options', function(){
+describe('randomValue() with options specified', function(){
 
     var returnValue
 
@@ -12,7 +28,7 @@ describe('randomValue() with default options', function(){
     it('should return an array of boolean', function(){
         returnValue = randomValue({'choice' : 1})
         expect(returnValue).to.be.an('array')
-        expect(returnValue).not.to.have.length.above(20)
+        expect(returnValue).to.have.length.within(randomOptions.arrayMin, randomOptions.arrayMax)
         for(var i = 0 ; i < returnValue.length ; i++){
             expect(returnValue[i]).to.be.a('boolean')
         }
@@ -25,10 +41,10 @@ describe('randomValue() with default options', function(){
     it('should return an array of integers', function(){
         returnValue = randomValue({'choice' : 3})
         expect(returnValue).to.be.an('array')
-        expect(returnValue).not.to.have.length.above(20)
+        expect(returnValue).to.have.length.within(randomOptions.arrayMin, randomOptions.arrayMax)
         for(var i = 0 ; i < returnValue.length ; i++){
             expect(returnValue[i]).to.be.a('number')
-            expect(returnValue[i]).not.to.be.above(1000)
+            expect(returnValue[i]).to.be.within(randomOptions.integerMin, randomOptions.integerMax)
         }
     })
     it('should return an float', function(){
@@ -39,54 +55,52 @@ describe('randomValue() with default options', function(){
     it('should return an array of floats', function(){
         returnValue = randomValue({'choice' : 5})
         expect(returnValue).to.be.an('array')
-        expect(returnValue).not.to.have.length.above(20)
+        expect(returnValue).to.have.length.within(randomOptions.arrayMin, randomOptions.arrayMax)
         for(var i = 0 ; i < returnValue.length ; i++){
             expect(returnValue[i]).to.be.a('number')
-            expect(returnValue[i]).not.to.be.above(1000)
+            expect(returnValue[i]).to.be.within(randomOptions.floatMin, randomOptions.floatMax)
         }
     })
     it('should return a date', function(){
         returnValue = randomValue({'choice' : 6})
         expect(returnValue).to.be.a('date')
-        var now = new Date()
-        expect(returnValue.valueOf()).not.to.be.above(now.valueOf())
+        expect(returnValue.valueOf()).to.be.within(randomOptions.dateStart.valueOf(), randomOptions.dateEnd.valueOf())
     })
     it('should return an array of dates', function(){
         returnValue = randomValue({'choice' : 7})
         expect(returnValue).to.be.an('array')
-        expect(returnValue).not.to.have.length.above(20)
-        var now = new Date()
+        expect(returnValue).to.have.length.within(randomOptions.arrayMin, randomOptions.arrayMax)
         for(var i = 0 ; i < returnValue.length ; i++){
             expect(returnValue[i]).to.be.a('date')
-            expect(returnValue[i].valueOf()).not.to.be.above(now.valueOf())
+            expect(returnValue[i].valueOf()).to.be.within(randomOptions.dateStart.valueOf(), randomOptions.dateEnd.valueOf())
         }
     })
     it('should return a string', function(){
         returnValue = randomValue({'choice' : 8})
         expect(returnValue).to.be.a('string')
-        expect(returnValue).not.to.have.length.above(20)
+        expect(returnValue).to.have.length.within(randomOptions.stringMin, randomOptions.stringMax)
     })
     it('should return an array of strings', function(){
         returnValue = randomValue({'choice' : 9})
         expect(returnValue).to.be.an('array')
-        expect(returnValue).not.to.have.length.above(20)
+        expect(returnValue).to.have.length.within(randomOptions.arrayMin, randomOptions.arrayMax)
         for(var i = 0 ; i < returnValue.length ; i++){
             expect(returnValue[i]).to.be.a('string')
-            expect(returnValue[i]).not.to.have.length.above(20)
+            expect(returnValue[i]).to.have.length.within(randomOptions.stringMin, randomOptions.stringMax)
         }
     })
     it('should return an object', function(){
         returnValue = randomValue({'choice' : 10})
         expect(returnValue).to.be.a('object')
-        expect(Object.keys(returnValue).length).not.to.be.above(8)
+        expect(Object.keys(returnValue).length).to.be.within(randomOptions.objectMin, randomOptions.objectMax)
     })
     it('should return an array of objects', function(){
         returnValue = randomValue({'choice' : 11})
         expect(returnValue).to.be.an('array')
-        expect(returnValue).not.to.have.length.above(20)
+        expect(returnValue).to.have.length.within(randomOptions.arrayMin, randomOptions.arrayMax)
         for(var i = 0 ; i < returnValue.length ; i++){
             expect(returnValue[i]).to.be.a('object')
-            expect(Object.keys(returnValue[i]).length).not.to.be.above(8)
+            expect(Object.keys(returnValue[i]).length).to.be.within(randomOptions.objectMin, randomOptions.objectMax)
         }
     })
 })
