46ae048ffc2808b1672d12f3b6a78764038726ab
rmyers <ccrider@whiterose.net>
2015-01-18 19:06:21 -0500

    fix for 'this' when including the function.

--- test/fuzz-without-seneca.js
+++ test/fuzz-without-seneca.js
@@ -1,12 +1,13 @@
 var     sinon       = require('sinon')
     ,   expect      = require('chai').expect
     ,   seneca      = { 'util' : { 'deepextend' : sinon.stub() }}
-    ,   done        = sinon.stub()
-    ,   fuzztester  = require('../')(seneca, {}, done)
 
 describe('fuzz tester plugin function', function(){
     it('should return the name of the plugin on instantiation', function(){
-        var returnValue = fuzztester()
-        expect(done.callCount).to.equal(1)
+        seneca.util.deepextend()
+        var returnValue = require('../').bind(seneca,seneca,{}, function(err, result){
+            expect(result).to.equal({'name' : 'fuzztester'})
+        })
+
     })
 })
